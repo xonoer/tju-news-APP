b6ed2d536a352d5f6d2b05511f37bfad9018feee
yangmutong <tjuymt@tju.edu.cn>
2016-05-30 15:08:57 +0800

    Update Select.java

--- j2ee/Select.java
+++ j2ee/Select.java
@@ -1,5 +1,3 @@
-package Project;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -13,11 +11,6 @@ import com.google.common.base.Functions;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 
 import org.bson.Document;
 
@@ -26,92 +19,32 @@ import java.util.List;
 
 public class Select extends HttpServlet {
 
-	/**
-	 * Constructor of the object.
-	 */
 	public Select() {
 		super();
 	}
-
-	/**
-	 * Destruction of the servlet. <br>
-	 */
 	public void destroy() {
-		super.destroy(); // Just puts "destroy" string in log
-		// Put your code here
+		super.destroy();
 	}
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		/*response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();*/
-		String typeString=request.getParameter("type");
-		String pageString=request.getParameter("page");
-		String numString=request.getParameter("num");
-		int page=Integer.parseInt(pageString);
-		int num=Integer.parseInt(numString);
+		String typeString = request.getParameter("type");
+		String pageString = request.getParameter("page");
+		String numString = request.getParameter("num");
+		int page = Integer.parseInt(pageString);
+		int num = Integer.parseInt(numString);
 		JsonArray jsonArray = functions.getData(typeString,page,num);
+
+		response.setContentType("application/json");
+		esponse.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();  
-        out.write(jsonArray.toString()); 
+        out.write(jsonArray);
+        out.flush();
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		/*response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the POST method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();*/
-	}
-
-	/**
-	 * Initialization of the servlet. <br>
-	 *
-	 * @throws ServletException if an error occurs
-	 */
-	public void init() throws ServletException {
-		// Put your code here
+		doGet(request,response);
 	}
 
 }
